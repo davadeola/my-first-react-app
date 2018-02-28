0ce75c12568d5c125e168c027171b2f419ca68aa
David Adeola <adeoladavid15@gmail.com>
2018-02-28 15:34:04 +0300

    Displaying two elements using the app div

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 var greeting = React.createElement('h1',{},'Hello WOrld');
+var clock = React.createElement('h2',{}, `It is ${new Date().toLocaleString()}`);
+var app = React.createElement('div',{}, greeting, clock);
 
-ReactDOM.render(greeting, document.getElementById('react-app-root'));
+ReactDOM.render(app, document.getElementById('react-app-root'));
