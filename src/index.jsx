6bb67bb2186444a476c3431d19eab1b9310b1822
David Adeola <adeoladavid15@gmail.com>
2018-02-28 21:29:19 +0300

    Added the webconfig setup plus npm environment

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from 'react-dom';
+
+
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3A</h3>
+    <h3>Thato and Haley</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
