613d568299745ac362e606f7b1cc98474ece8696
David Adeola <adeoladavid15@gmail.com>
2018-03-01 12:07:21 +0300

    Added a header component and placed it in app.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header"
+
+function App(){
+  return (
+    <div>
+    <Header/>
+      <h3>3A</h3>
+      <h3>Thato and Haley</h3>
+      <p>
+        <em>Firebase will not save record!</em>
+      </p>
+    </div>
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import ReactDom from 'react-dom';
-
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx"
 
 ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h3>3A</h3>
-    <h3>Thato and Haley</h3>
-    <p><em>Firebase will not save record!</em></p>
-  </div>,
+  <App/>,
   document.getElementById('react-app-root')
 );
