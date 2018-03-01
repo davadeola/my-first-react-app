4f1e4ace0b83abf99673f8b44d892173e840637f
David Adeola <adeoladavid15@gmail.com>
2018-03-01 13:12:38 +0300

    Added multiple components and shared their functionality across

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Ticket(){
+  return(
+    <div>
+      <h3>3A</h3>
+      <h3>Thato and Haley</h3>
+      <p>
+        <em>Firebase will not save record!</em>
+      </p>
+    </div>
+  );
+}
+
+export default Ticket;
