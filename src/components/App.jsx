d59fd2f388c87f3f07219d02265c39f83da792df
David Adeola <adeoladavid15@gmail.com>
2018-03-13 14:46:41 +0300

    Created my first stateful class based component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import Header from "./Header";
 import TicketList from "./TicketList";
 import {Switch, Route} from "react-router-dom";
-import NewTicketForm from "./NewTicketForm";
-
+import NewTicketControl from "./NewTicketControl";
+import Error404 from "./Error404";
 function App(){
   return (
     <div>
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList}/>
-        <Route path='/newticket' component={NewTicketForm}/>
+        <Route path='/newticket' component={NewTicketControl}/>
+        <Route component={Error404}/>
       </Switch>
     </div>
   );
