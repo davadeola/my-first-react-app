897eca085d001236435e2cabe41da1a875b8e118
David Adeola <adeoladavid15@gmail.com>
2018-03-28 23:11:20 +0300

    Added a dummy test i ticket-list-reducer.test.js just to see how to write a jest test

---
+++ _tests_/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+
+describe("ticketlistReducer", ()=> {
+  // body...
+  test("Temporary : two plus two equals four", ()=>{
+    expect(2+2).toBe(0);
+  });
+});
+
+/*eslint-disable*/
