601577195cf0830d4077fc3bb290c824f3d627c8
David Adeola <adeoladavid15@gmail.com>
2018-03-29 11:59:15 +0300

    Replaced AppConatiner with the Provider component imported from react-redux then I added my store as a prop to Provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import {AppContainer} from "react-hot-loader";
+//import {AppContainer} from "react-hot-loader";
 import {HashRouter} from "react-router-dom";
 import {createStore} from "redux";
 import ticketListReducer from "./reducers/ticket-list-reducer";
+import {Provider} from "react-redux";
 
 const store = createStore(ticketListReducer);
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <HashRouter>
+    <HashRouter>
+      <Provider store={store}>
         <Component/>
-      </HashRouter>
-    </AppContainer>, document.getElementById("react-app-root"));
+      </Provider>
+    </HashRouter>, document.getElementById("react-app-root"));
 };
 
 render(App);
 
 /*eslint-disable*/
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    render(App)
-  });
-}
-/*eslint-disable*/
+  if (module.hot) {
+    module.hot.accept('./components/App', () => {
+      render(App)
+    });
+  }
+  /*eslint-disable*/
